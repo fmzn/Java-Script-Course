fc45dc1a07ab6ddddbdc09e952f7ca5c8291c28e
Kaori Fumizono <fumizonok@gmail.com>
2021-07-30 00:41:56 +0900

    reduce関数

---
+++ 04_JavaScriptの基礎を固める/074_コールバック関数とループ/start/main.js
@@ -0,0 +1,11 @@
+const array = [1,2,3,4,5];
+
+function forEach(ary) {
+  for(let i =0; i <ary.length; i++) {
+    console.log(ary[i]);
+  }
+}
+
+function log()
+
+forEach(array);
---
+++ 04_JavaScriptの基礎を固める/076_配列とforEachメソッド/start/main.js
@@ -0,0 +1,6 @@
+const array = [1,2,3,4,5];
+
+array.forEach(function(v, i, ary) {
+  console.log(v, i, ary);
+
+})
