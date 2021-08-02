d0b732387058526a0fd2c4a3f0cbad671e546072
Kaori Fumizono <fumizonok@gmail.com>
2021-08-03 00:21:48 +0900

    javascript

--- 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
+++ 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
@@ -1,9 +1,21 @@
-const str = 'animation';
-const strArry = str.split('');
+const str = "animation";
+const strArry = str.split("");
 
 function tag(accu, curr) {
+    console.log(accu,curr);
     return `${accu}<${curr}>`;
 }
 
-const result = strArry.reduce(tag, "");
+function reduce(arry, callback, defaultValue) {
+    let accu = defaultValue;
+    
+    arry.forEach((letter) => {
+        let curr = letter;
+        accu = callback(accu,curr);
+    });
+
+    return accu;
+}
+
+const result = reduce(strArry, tag, "");
 console.log(result);
