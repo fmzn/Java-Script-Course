d82552f165894e4e74cd458791ae961c59d48399
Kaori Fumizono <fumizonok@gmail.com>
2021-08-06 00:15:49 +0900

    javascript

---
+++ 04_JavaScriptの基礎を固める/090_DOMにイベントを登録してみよう/start/main.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector('#btn');
+const h1 = document.querySelector('h1');
+
+function changeColor() {
+    h1.style.color = 'red';
+};
+function changeBgColor() {
+  h1.style.backgroundColor = 'green';
+  console.log('hello')
+};
+btn.addEventListener('click', changeColor);
+btn.addEventListener('click', changeBgColor);
+
+// btn.onclick = changeColor;
